08d517a581ae8ee822d8177710e139017e8645a4
deepfl <xxl124730@utdallas.edu>
2019-05-06 02:02:02 -0500

    update run deepfl script

--- run_deepfl.sh
+++ run_deepfl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 subs=(Chart Lang Time Math Mockito Closure)
 vers=(26 65 27 106 38 133)
-for idx in {0..6}
+for idx in {0..5}
 do
 	sub=${subs[$idx]}
 	for model in {mlp,mlp2,birnn,dfl1,dfl2}
