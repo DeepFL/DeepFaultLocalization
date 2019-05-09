5a0289301f04a5d27345b1c6712fc9f86c6ee341
wadmes <werry715@gmail.com>
2019-05-09 16:02:56 +0800

    test

--- ResultAnalysis/result_main.py
+++ ResultAnalysis/result_main.py
@@ -45,8 +45,8 @@ def main():
 		subprocess.call('Rscript RforRQ3.r loss_eval',shell=True)
   	
   	if RQ_number == "RQ4":
-  		if "paper" not in result_dir:
-  			result.cross_vali_result()	
+  		#if "paper" not in result_dir:
+  		result.cross_vali_result()	
   			
 		techs = ['DeepFL','CrossDeepFL','CrossValidation']
 		model_name = ['MLP_DFL(2)']   #not important
