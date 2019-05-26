bccb385ef9bd34919dd8a08d7fdb724d514ae1f5
deepfl <xxl124730@utdallas.edu>
2019-05-26 11:49:43 -0500

    update

--- FeatureExtraction/Scripts/RawFeatures/MutationFeature.sh
+++ FeatureExtraction/Scripts/RawFeatures/MutationFeature.sh
@@ -3,6 +3,9 @@ rootPath=$1
 
 cd $rootPath/SubjectExample/Chart/1
 mvn org.pitest:pitest-maven:mutationCoverage -f pomMutation.xml
+
+rm -rf $rootPath/RawFeatures/Mutation/Chart/mutation-test/1.txt
+rm -rf $rootPath/RawFeatures/Mutation/Chart/coverage-test/1.txt
 cd mutation-test
 for file in *.gz; do gzip -c -d "$file">>$rootPath/RawFeatures/Mutation/Chart/mutation-test/1.txt; done
 cd ../coverage-test
