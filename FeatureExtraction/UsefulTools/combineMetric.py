9851626a82ff7ae5e7d646feb9eb59169c727572
deepfl <xxl124730@utdallas.edu>
2019-05-26 11:19:29 -0500

    combine metric feature

--- FeatureExtraction/UsefulTools/combineMetric.py
+++ FeatureExtraction/UsefulTools/combineMetric.py
@@ -2,15 +2,21 @@ import sys
 from collections import defaultdict
 root_path = sys.argv[1]
 
-source_result = root_path + "/FeatureValues/Complexity/Chart/1.txt"
+source_result = root_path + "/FeatureValues/Complexity/Chart/1source.txt"
 byte_result = root_path + "/FeatureValues/Complexity/Chart/1byte.txt"
 
-source_result_map = defaultdict(str)
 comb_result_map = defaultdict(str)
 
 with open(source_result) as sr:
 	for line in sr:
 		method_name = line.split(" ")[0]
 		vs = line.split(" ")[1].strip()
-		source_result_map[method_name] = vs
-print(source_result_map)		
+		comb_result_map[method_name] = vs
+with open(byte_result) as br:
+	for line in br:
+		method_name = line.split(" ")[0]
+		vs = line.split(" ")[1].strip()
+		if method_name in comb_result_map:
+			comb_result_map[method_name] = comb_result_map[method_name] + vs
+
+comb_result_path = root_path + "/FeatureValues/Complexity/Chart/1.txt"
