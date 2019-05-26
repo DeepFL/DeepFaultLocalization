ff28da502ba967498b3f2493a648440fa64542e3
deepfl <xxl124730@utdallas.edu>
2019-05-26 07:33:51 -0500

    don't install

--- FeatureExtraction/Scripts/RawFeatures/SpectrumFeature.sh
+++ FeatureExtraction/Scripts/RawFeatures/SpectrumFeature.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 rootPath=$1
-cd $rootPath/UsefulTools/PIT-Spectrum
-mvn clean install -Dmaven.test.skip=true
+
 cd $rootPath/SubjectExample/Chart/1
 mvn clean test
 mvn org.pitest:pitest-maven:mutationCoverage -f pomSpectrum.xml
