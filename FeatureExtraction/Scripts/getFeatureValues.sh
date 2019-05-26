3aa09db022659f86848268f1dcac30d6b07e7294
deepfl <xxl124730@utdallas.edu>
2019-05-26 08:47:16 -0500

    add path

--- FeatureExtraction/Scripts/getFeatureValues.sh
+++ FeatureExtraction/Scripts/getFeatureValues.sh
@@ -3,13 +3,13 @@ rootPath=$1
 
 # get Spectrum and mutation
 cd $rootPath/UsefulTools/mutationfl/bin
-java Main.RunMain mutation 1 Chart
+java Main.RunMain mutation 1 Chart $rootPath
 cd $rootPath/UsefulTools/
 python convertSpecAndMutation.py $rootPath
 
 # get source code complexity values
 cd $rootPath/UsefulTools/mutationfl/bin
-java Main.RunMain MetricData 1 Chart
+java Main.RunMain MetricData 1 Chart $rootPath
 
 # just copy byte code complexity values from RawFeatures
 cp $rootPath/RawFeatures/Complexity/Chart/1byte.txt $rootPath/FeatureValues/Complexity/Chart/
