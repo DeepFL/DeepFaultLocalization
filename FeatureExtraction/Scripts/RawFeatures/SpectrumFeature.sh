8b79cee27ce8302927a07c8b4a262f3f3cd02665
deepfl <xxl124730@utdallas.edu>
2019-05-26 07:49:16 -0500

    update mvn clean

--- FeatureExtraction/Scripts/RawFeatures/SpectrumFeature.sh
+++ FeatureExtraction/Scripts/RawFeatures/SpectrumFeature.sh
@@ -2,6 +2,6 @@
 rootPath=$1
 
 cd $rootPath/SubjectExample/Chart/1
-mvn clean test
+mvn clean test -f pomSpectrum.xml
 mvn org.pitest:pitest-maven:mutationCoverage -f pomSpectrum.xml
 cp line-assert/line-assert_detail $rootPath/RawFeatures/Spectrum/Chart/1.txt
