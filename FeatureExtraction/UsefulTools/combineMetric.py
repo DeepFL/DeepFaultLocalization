0396dd2ecfce3ad40b9c4c0207601547da91dca9
deepfl <xxl124730@utdallas.edu>
2019-05-26 12:07:13 -0500

    update combine metric

--- FeatureExtraction/UsefulTools/combineMetric.py
+++ FeatureExtraction/UsefulTools/combineMetric.py
@@ -17,6 +17,7 @@ with open(byte_result) as br:
 	for line in br:
 		method_name = line.split(" ")[0]
 		vs = line.split(method_name + " ")[1].strip().replace(" ",",")
+		print(vs)
 		if method_name in comb_result_map:
 			comb_result_map[method_name] = source_result_map[method_name] + vs
 
