2bbb13277237ad4cc5da152e03b669cb8d15c12d
wadmes <werry715@gmail.com>
2019-05-26 22:23:52 +0800

    modify to fix bug

--- RunDeepFL/main.py
+++ RunDeepFL/main.py
@@ -1,10 +1,8 @@
 import bidirectional_rnn as birnn
-import recurrent_network as myrnn
 import multilayer_perceptron_one_hidden_layer as mlp
 import multilayer_perceptron_two_hidden_layer as mlp2
 import fc_based_1 as dfl1
 import fc_based_2 as dfl2
-import fc_based as fc
 import os
 import time
 import numpy as np
@@ -29,9 +27,9 @@ def main():
     l = losses.index(loss)   #get index of loss function
     start_time = time.time()
     susp_path = os.path.join(susp_dir, model + '-' + losses[l])
-    if model == "rnn":
-        myrnn.run(train_path,train_label_path, test_path,test_label_path, group_path, susp_path, featureDistribution, l)
-    elif model == "birnn":
+    # if model == "rnn":
+    #     myrnn.run(train_path,train_label_path, test_path,test_label_path, group_path, susp_path, featureDistribution, l)
+    if model == "birnn":
         birnn.run(train_path,train_label_path, test_path,test_label_path, group_path, susp_path, featureDistribution, l)
     elif model == "mlp":
         mlp.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
@@ -42,10 +40,10 @@ def main():
     elif model == "dfl2":
         dfl2.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
         #mlp2.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
-    elif model == "fc":
-        print(sub + '-' + v)
-        fc.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
-        #mlp2.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
+    # elif model == "fc":
+    #     print(sub + '-' + v)
+    #     fc.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
+    #     #mlp2.run(train_path,train_label_path, test_path,test_label_path, group_path ,susp_path, l, featureNum=feature,nodeNum=feature)
     end_time = time.time()
         
     #print("--- %s %s %s time: %s seconds ---" % (model, tech, losses[l], (end_time - start_time)))
