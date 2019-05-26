7c21107a772cb893c0d2374d52869d9645260fa5
deepfl <xxl124730@utdallas.edu>
2019-05-26 09:38:37 -0500

    change path in code

--- FeatureExtraction/UsefulTools/Textual/GetSusFromInformR.java
+++ FeatureExtraction/UsefulTools/Textual/GetSusFromInformR.java
@@ -11,7 +11,7 @@ public class GetSusFromInformR {
 		String ID = args[0];
 		String Project = args[1];
 		String rootPath = args[2];
-		BufferedReader MF = new BufferedReader(new FileReader(rootPath + "FeatureValues/SpectrumandMutation/"+Project+"/"+ID+"/Ample.txt"));
+		BufferedReader MF = new BufferedReader(new FileReader(rootPath + "FeatureValues/tempResult/"+Project+"/"+ID+"/Ample.txt"));
 		String methodsName = MF.readLine();
 		while(methodsName != null){
 			MethodsCoveredbyF.add(methodsName.split(" ")[0]);
